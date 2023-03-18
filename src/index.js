9a75318476d170b498929fec237a3a0a17470e9b
Jason <121495300+jason21715@users.noreply.github.com>
2023-03-18 04:08:30 -0700

    Converting this to a todo list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+const content = document.getElementById("content");
+const h1 = document.createElement("h1");
+const p = document.createElement("p");
+
 export function createNavBar() {
-  const content = document.getElementById("content");
   const newNav = document.createElement("nav");
   const newUl = document.createElement("ul");
   content.appendChild(newNav.appendChild(newUl));
@@ -14,15 +17,32 @@ export function createNavBar() {
   }
 }
 
-// function createHomePage() {
-//   const content = document.getElementById("content");
-//   const newDiv = document.createElement("div");
-// }
+export function createHomePage() {
+  let titles = ["Welcome", "Hours", "Location"];
+  let descriptions = [
+    "Welcome to Jason's Restauraunt, we serve all kinds of cheeses such as: Fermundo Cheese, Bermundo Cheese, Deluxe Yeast, Mega premium cheese. Please Enjoy Our Premium cheeses imported from all over the world!!",
+    "MON: 1AM-1PM<br> TUE: 1AM-1PM<br>  WED: 1AM-1PM<br> THU: 1AM-1PM<br> FRI: 1AM-1PM<br> SAT: 1AM-1PM<br> SUN: 1AM-1PM<br>",
+    "12491 RatouttileJr Street. City: New York Zip: 41212",
+  ];
+  const newDiv = document.createElement("div");
+  newDiv.id = "homePage";
+  content.appendChild(newDiv);
+  const homePage = document.getElementById("homePage");
+  for (let i = 0; i < titles.length; i++) {
+    const newHomePageDivs = document.createElement("div");
+    newHomePageDivs.id = `homePageBox${i}`;
+    homePage.appendChild(newHomePageDivs);
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+    h1.textContent = titles[i];
+    p.innerHTML = descriptions[i];
+    newHomePageDivs.appendChild(h1);
+    newHomePageDivs.appendChild(p);
+  }
+}
 
-// function createMenuPage() {
-//   const content = document.getElementById("content");
-// }
+export function createMenuPage() {}
 
-// function createContactPage() {
+// export function createContactPage() {
 //   const content = document.getElementById("content");
 // }
