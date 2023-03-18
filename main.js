6608cb7859e430403b12cf1844f0096f7a6c67cc
Jason <121495300+jason21715@users.noreply.github.com>
2023-03-18 04:08:30 -0700

    Converting this to a todo list

--- main.js
+++ main.js
@@ -2,6 +2,20 @@
   "use strict";
 })();
 
-import { createNavBar } from "../src/index.js";
+import { createNavBar, createHomePage } from "../src/index.js";
 
 createNavBar();
+
+function navBarButtonFunctions() {
+  const home = document.getElementById("Home");
+  const menu = document.getElementById("Menu");
+  const contact = document.getElementById("Contact");
+
+  home.addEventListener("click", () => {});
+
+  menu.addEventListener("click", () => {});
+
+  contact.addEventListener("click", () => {});
+}
+
+createHomePage();
