4a1d1931606bf189329ffa022a269452105f2ea6
Jason <121495300+jason21715@users.noreply.github.com>
2023-03-19 02:09:35 -0700

    Add welcome message

--- src/index.js
+++ src/index.js
@@ -1,48 +1,7 @@
-const content = document.getElementById("content");
-const h1 = document.createElement("h1");
-const p = document.createElement("p");
+//idk anymore
+const usernameInput = document.getElementById("usernameInput");
 
-export function createNavBar() {
-  const newNav = document.createElement("nav");
-  const newUl = document.createElement("ul");
-  content.appendChild(newNav.appendChild(newUl));
-
-  const navBarContent = ["Home", "Menu", "Contact"];
-
-  for (const i of navBarContent) {
-    const newLi = document.createElement("li");
-    newLi.textContent = i;
-    newLi.id = i;
-    newUl.appendChild(newLi);
-  }
+export function getUserName() {
+  const username = usernameInput.value;
+  return username;
 }
-
-export function createHomePage() {
-  let titles = ["Welcome", "Hours", "Location"];
-  let descriptions = [
-    "Welcome to Jason's Restauraunt, we serve all kinds of cheeses such as: Fermundo Cheese, Bermundo Cheese, Deluxe Yeast, Mega premium cheese. Please Enjoy Our Premium cheeses imported from all over the world!!",
-    "MON: 1AM-1PM<br> TUE: 1AM-1PM<br>  WED: 1AM-1PM<br> THU: 1AM-1PM<br> FRI: 1AM-1PM<br> SAT: 1AM-1PM<br> SUN: 1AM-1PM<br>",
-    "12491 RatouttileJr Street. City: New York Zip: 41212",
-  ];
-  const newDiv = document.createElement("div");
-  newDiv.id = "homePage";
-  content.appendChild(newDiv);
-  const homePage = document.getElementById("homePage");
-  for (let i = 0; i < titles.length; i++) {
-    const newHomePageDivs = document.createElement("div");
-    newHomePageDivs.id = `homePageBox${i}`;
-    homePage.appendChild(newHomePageDivs);
-    const h1 = document.createElement("h1");
-    const p = document.createElement("p");
-    h1.textContent = titles[i];
-    p.innerHTML = descriptions[i];
-    newHomePageDivs.appendChild(h1);
-    newHomePageDivs.appendChild(p);
-  }
-}
-
-export function createMenuPage() {}
-
-// export function createContactPage() {
-//   const content = document.getElementById("content");
-// }
