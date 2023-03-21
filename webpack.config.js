2ec6ef2f827dca442782a8cdcf4b65f73fd5e5a0
Jason <121495300+jason21715@users.noreply.github.com>
2023-03-21 06:09:22 -0700

    Github Page Test #4

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,53 @@
-const path = require('path');
+/* eslint-disable no-undef */
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
 };
+
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+//   entry: {
+//     index: "./src/index.js",
+//     main: "./src/main.js",
+//     get: "./src/get.js",
+//   },
+
+//   output: {
+//     filename: "[name].bundle.js",
+//     path: path.resolve(__dirname, "dist"),
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: "Output Management",
+//     }),
+//   ],
+// };
